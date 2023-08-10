403dc7f504c452cb9594d1279dbe0cd53e2fd0f9
GILAD SAMUEL <giladsam98@gmail.com>
2023-08-10 22:59:48 +0300

    fixed number range bug, now can change the num in cell or delete it

--- View/custom_sudoku_ui.py
+++ View/custom_sudoku_ui.py
@@ -24,57 +24,72 @@ class CustomMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.PB
         button_map = GenButtonMap()
 
-        for row in range(9):
-            for col in range(9):
+        for row in range(1, 10):
+            for col in range(1, 10):
                 button_name = f"PB_{row}{col}"
                 button = getattr(self, button_name)
-                button.clicked.connect(lambda checked, i=i, j=j: self.cell_clicked(i, j))
-
-        
-
-        # Maps to map commands, keys and functions
-        self.key_table = {k: Cmds.NUM for k in range(QtCore.Qt.Key_0, QtCore.Qt.Key_9+1)}
-        self.key_table.update({QtCore.Qt.Key_Backspace: Cmds.DEL})
-        self.func_map = {}
-
-        # Variables (UI)
-        self.cells = self.CreateBoard(self)
+                button.clicked.connect(lambda checked, row=row, col=col: self.cell_clicked(row, col))
 
     def cell_clicked(self, row, col):
-        def key_pressed(event):
+
+        BACKSPACE_KEY = '\b'
+        DELETE_KEY = '\x7f'
+        ZERO_KEY = '0'
+
+        def key_pressed(button, event):
             key = event.text()
             if key.isdigit() and 1 <= int(key) <= 9:
-                # Update the button's text and handle your Sudoku logic
-                button = getattr(self, f"PB_{row}{col}")
                 button.setText(key)
                 # Add your logic to update the underlying data structure for Sudoku
+            elif key in (ZERO_KEY, BACKSPACE_KEY, DELETE_KEY):
+                button.setText("")
 
         button = getattr(self, f"PB_{row}{col}")
-        button.setFocus()  # Make sure the button has focus
-        button.keyPressEvent = key_pressed        
+        button.setFocus()
+        button.keyPressEvent = lambda event, button=button: key_pressed(button, event)
 
-    @staticmethod
-    def CreateBlock(parent, layout, bi, bj):
-        block = Block(parent)
-        layout.addWidget(block, bi, bj)
-        return block
-
-    @staticmethod
-    def CreateCell(i, j, boxes, click_func):
-        bi, bj = i // 3, j // 3
-        parent_box = boxes[bi][bj]
-        cell = Cell(parent_box, i, j)
-        cell.ConnectCelltoWindow(click_func)
-        parent_box.AddCell(cell, i - 3*bi, j - 3*bj)
-        return cell
-    
-    def CreateBoard(self, parent, layout):
-        """ Creates board display with initial board values and candidates """
-        # Create boxes for each 9x9 block
-        blocks = [[self.CreateBlock(parent, layout, bi, bj) for bj in range(3)] for bi in range(3)]
-        return [[self.CreateCell(i, j, blocks, self.CellClicked) for j in range(9)] for i in range(9)]
-
-        
+    #     # Maps to map commands, keys and functions
+    #     self.key_table = {k: Cmds.NUM for k in range(QtCore.Qt.Key_0, QtCore.Qt.Key_9+1)}
+    #     self.key_table.update({QtCore.Qt.Key_Backspace: Cmds.DEL})
+    #     self.func_map = {}
+    #
+    #     # Variables (UI)
+    #     self.cells = self.CreateBoard(self)
+    #
+    # def cell_clicked(self, row, col):
+    #     def key_pressed(event):
+    #         key = event.text()
+    #         if key.isdigit() and 1 <= int(key) <= 9:
+    #             # Update the button's text and handle your Sudoku logic
+    #             button = getattr(self, f"PB_{row}{col}")
+    #             button.setText(key)
+    #             # Add your logic to update the underlying data structure for Sudoku
+    #
+    #     button = getattr(self, f"PB_{row}{col}")
+    #     button.setFocus()  # Make sure the button has focus
+    #     button.keyPressEvent = key_pressed
+    #
+    # @staticmethod
+    # def CreateBlock(parent, layout, bi, bj):
+    #     block = Block(parent)
+    #     layout.addWidget(block, bi, bj)
+    #     return block
+    #
+    # @staticmethod
+    # def CreateCell(i, j, boxes, click_func):
+    #     bi, bj = i // 3, j // 3
+    #     parent_box = boxes[bi][bj]
+    #     cell = Cell(parent_box, i, j)
+    #     cell.ConnectCelltoWindow(click_func)
+    #     parent_box.AddCell(cell, i - 3*bi, j - 3*bj)
+    #     return cell
+    #
+    # def CreateBoard(self, parent, layout):
+    #     """ Creates board display with initial board values and candidates """
+    #     # Create boxes for each 9x9 block
+    #     blocks = [[self.CreateBlock(parent, layout, bi, bj) for bj in range(3)] for bi in range(3)]
+    #     return [[self.CreateCell(i, j, blocks, self.CellClicked) for j in range(9)] for i in range(9)]
+    #
+    #
